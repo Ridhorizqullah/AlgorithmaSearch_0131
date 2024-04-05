8e36e1548e79634aff15a7e13de23db00d1914f4
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-04-05 11:03:26 +0700

    if

--- AlgorithmaSearch_0131/AlgorithmaSearch_0131.cpp
+++ AlgorithmaSearch_0131/AlgorithmaSearch_0131.cpp
@@ -36,7 +36,13 @@ void LinearSearch()
         i = 0; // step 2
         while (i < n) // step 3
         {
-            
+            ctr++;
+            if (arr[i] == item)
+            {
+                cout << "\n" << item << "found at position " << (i + 1) << endl; // step 6
+                break;
+            }
+            i++; // step 4
 
         }
         if (i == 0) // step 5
