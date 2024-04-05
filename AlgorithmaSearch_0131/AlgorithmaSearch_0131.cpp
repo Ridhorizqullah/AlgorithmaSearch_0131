382c1dff4cf0b613779f7e0eb076091e0ce6d663
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-04-05 11:02:50 +0700

    do while

--- AlgorithmaSearch_0131/AlgorithmaSearch_0131.cpp
+++ AlgorithmaSearch_0131/AlgorithmaSearch_0131.cpp
@@ -26,7 +26,16 @@ void LinearSearch()
     int ctr; // number of comparisons
     int item;
 
-    
+    do
+    {
+        //accept the number to be searched
+        cout << "\nEnter the element you want to search: "; // step 1
+        cin >> item;
+
+        
+
+    } while ((ch == 'y') || (ch == 'Y'));
+
 }
 
 
